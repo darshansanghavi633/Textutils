66c57a8ac2a4802848e98c0110ec06adc180bc31
Darshan Sanghavi <darshansanghavi0505@gmail.com>
2023-01-05 18:02:30 +0530

    fixing small bugs

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -41,20 +41,20 @@ export default function TextForm(props) {
     <div className='container' style={{color: props.mode==='light'?'#042743':'white'}}>
         <h1>Enter the text to analyze</h1>
         <div className="mb-3">
-        <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='light'?'white':'grey', color:  props.mode==='light'?'#042743':'white'}} id="myBox" rows="8"></textarea>
+        <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='light'?'white':'#13466e', color:  props.mode==='light'?'#042743':'white'}} id="myBox" rows="8"></textarea>
         </div>
-        <button className="btn btn-primary mx-3" onClick={handleUpClick}>Convert to Uppercase</button>
-        <button className="btn btn-primary mx-3" onClick={handleLoClick}>Convert to Lowercase</button>
-        <button className="btn btn-primary mx-3" onClick={handleClearClick}>Click to Clear</button>
-        <button className="btn btn-primary mx-3" onClick={handleCopyText}>Copy Text</button>
-        <button className="btn btn-primary mx-3" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
+        <button disabled={text.length===0} className="btn btn-primary mx-3 my-1" onClick={handleUpClick}>Convert to Uppercase</button>
+        <button disabled={text.length===0} className="btn btn-primary mx-3 my-1" onClick={handleLoClick}>Convert to Lowercase</button>
+        <button disabled={text.length===0} className="btn btn-primary mx-3 my-1" onClick={handleClearClick}>Click to Clear</button>
+        <button disabled={text.length===0} className="btn btn-primary mx-3 my-1" onClick={handleCopyText}>Copy Text</button>
+        <button disabled={text.length===0} className="btn btn-primary mx-3 my-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
     </div>
     <div className="container my-3" style={{color: props.mode==='light'?'#042743':'white'}}>
-        <h2>Text Sumarry</h2>
-        <p>{text.split(" ").length-1}words and {text.length}characters</p>
-        <p>{0.008 * (text.split(" ").length-1)}Minutes read time</p>
+        <h2>Text Sumary</h2>
+        <p>{text.split(" ").filter((element)=>{return element.length!==0}).length}words and {text.length}characters</p>
+        <p>{0.008 * (text.split(" ").filter((element)=>{return element.length!==0}).length)}Minutes read time</p>
         <h2>Preview</h2>
-        <p>{text.length>0?text:"Enter something in the textbox above to  preview it"}</p>
+        <p>{text.length>0?text:"Nothing to preview!"}</p>
     </div>
     </>
   )
